8e94337eb5f1bac4a6f3e9a1038f3c65298d2de9
Hp <juanm.rivera@udea.edu.co>
2023-03-19 15:48:55 -0500

    Comienzo Practica 2 punto 1.

---
+++ app/src/main/java/com/juanrivera/moneda_vm/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.juanrivera.moneda_vm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juanrivera.moneda_vm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
